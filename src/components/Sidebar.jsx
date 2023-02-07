d3b69a6b671246f77585e82a77f9eb45aa82928b
saira512dev <512saira@gmail.com>
2023-02-07 20:51:43 +0400

    User profile is shown in the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -91,6 +91,7 @@ const navItems = [
 ]
 
 const Sidebar = ({
+    user,
     drawerWidth,
     isSidebarOpen,
     setIsSidebarOpen,
@@ -175,7 +176,29 @@ const Sidebar = ({
                  </List>
 
              </Box>
-
+            <Box position="absolute" bottom="2rem">
+                <Divider />
+                <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
+                    <Box
+                    component="img"
+                    alt="profile"
+                    src={ProfileImage}
+                    height="40px"
+                    width="40px"
+                    borderRadius="50%"
+                    sx={{ objectFit: "Cover"}} 
+                   />
+                    <Box textAlign="left">
+                        <Typography fontWeight="bold" fontSize="0.9rem" sx={{ color: theme.palette.secondary[100]}}>
+                            {user.name}
+                        </Typography>
+                        <Typography  fontSize="0.8rem" sx={{ color: theme.palette.secondary[200]}}>
+                            {user.occupation}
+                        </Typography>
+                    </Box>
+                    <SettingsOutlined sx={{color: theme.palette.secondary[300], fontSize: "25px"}} />
+                </FlexBetween>
+            </Box>
             </Drawer>
         )} 
     </Box>
