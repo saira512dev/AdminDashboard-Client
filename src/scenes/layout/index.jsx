e7dc56760120895421afb5bc9a05b393980d761e
saira512dev <512saira@gmail.com>
2023-02-07 20:51:27 +0400

    Redux toolkit query is used to fetch user data,

--- src/scenes/layout/index.jsx
+++ src/scenes/layout/index.jsx
@@ -4,19 +4,25 @@ import { useSelector } from "react-redux";
 import Navbar from "../../components/Navbar";
 import Sidebar from "../../components/Sidebar";
 import { Outlet } from "react-router-dom";
+import { useGetUserQuery  } from '../../state/api';
 
 const Layout = () => {
   const isNonMobile = useMediaQuery("(min-width: 600px)");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+  const userId = useSelector((state) => state.global.userId);
+  const { data } = useGetUserQuery(userId);
+  
   return ( 
   <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%" >
-      <Sidebar isNonMobile={isNonMobile}
+      <Sidebar
+      user={data || { }}
+      isNonMobile={isNonMobile}
       drawerWidth="250px"
       isSidebarOpen={isSidebarOpen}
       setIsSidebarOpen={setIsSidebarOpen}
       />
       <Box>
-        <Navbar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
+        <Navbar user={data || { }} isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
         <Outlet />
       </Box>
   </Box>
--- src/state/index.js
+++ src/state/index.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     mode: "dark",
-    userId: "63701cc1f03239b700000e",
+    userId: "63701cc1f03239b7f700000e",
 }
 
 export const globalSlice = createSlice({
