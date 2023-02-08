ee9b83af9e8e23a1dc62314ed3f18b613629ffae
saira512dev <512saira@gmail.com>
2023-02-08 23:04:49 +0400

    Product component added

---
+++ src/scenes/Products/index.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { 
+    Box,
+    Card,
+    CardActions,
+    CardContent,
+    Collapse,
+    Button,
+    Typography,
+    Rating,
+    useTheme,
+    useMediaQuery
+} from "@mui/material";
+import { useGetProductsQuery } from "../../state/api";
+import Header from "../../components/Header";
+
+const Products = () => {
+    return <div>Products</div>
+}
+
+export default Products
