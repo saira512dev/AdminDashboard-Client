1128de0ad23edf395862c905101dab33e1535b7f
saira512dev <512saira@gmail.com>
2023-02-10 23:28:49 +0400

    Routes to customers, transactions and geodata added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import { themeSettings } from "./theme";
 import  Dashboard  from "./scenes/dashboard";
 import  Layout  from "./scenes/layout";
 import  Products  from "./scenes/Products";
+import  Customers  from "./scenes/Customers";
+import  Transactions  from "./scenes/Transactions";
+import  Geography  from "./scenes/Geography";
 
 function App() {
   const mode = useSelector((state) => state.global.mode)
@@ -20,9 +23,11 @@ function App() {
             <Route path="/" element={<Navigate to="/dashboard" replace />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/products" element={<Products />} />
+            <Route path="/customers" element={<Customers />} />
+            <Route path="/transactions" element={<Transactions />} />
+            <Route path="/geography" element={<Geography />} />
           </Route>    
         </Routes>
-        
       </ThemeProvider>
     </BrowserRouter>
     
