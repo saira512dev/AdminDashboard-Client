ac88a82c3bf94d4db5e3e5173cbcec286b7f08e2
saira512dev <512saira@gmail.com>
2023-02-10 23:12:13 +0400

    api calls for accessing customers, transactions and country data added

--- src/state/api.js
+++ src/state/api.js
@@ -13,7 +13,23 @@ export const api = createApi({
         getProducts: build.query({ 
             query: () => "client/products",
             providesTags: ["Products"]
-        })
+        }),
+        getCustomers: build.query({ 
+            query: () => "client/customers",
+            providesTags: ["Customers"]
+        }),
+        getTransactions: build.query({ 
+            query: ({ page, pageSize, sort, search }) => ({
+                url:"client/transactions",
+                method: "GET",
+                params: { page, pageSize, sort, search},
+            }),
+            providesTags: ["Transactions"]
+        }),
+        getGeography: build.query({ 
+            query: () => "client/geography",
+            providesTags: ["Geography"]
+        }),
     })
 })
-export const { useGetUserQuery, useGetProductsQuery } = api;
+export const { useGetUserQuery, useGetProductsQuery, useGetCustomersQuery, useGetTransactionsQuery, useGetGeographyQuery } = api;
