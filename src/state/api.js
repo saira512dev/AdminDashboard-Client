2931695b4d7b0d48d299cef1b91b7da0baa99540
saira512dev <512saira@gmail.com>
2023-02-14 09:08:23 +0400

    api call to access sales data added

--- src/state/api.js
+++ src/state/api.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: `${process.env.REACT_APP_BASE_URL}` }),
     reducePath: "adminApi",
-    tagTypes: ["User"],
+    tagTypes: ["User", "Products", "Customers", "Transactions", "Geography", "Sales"],
     endpoints: (build) => ({
         getUser: build.query({ 
             query: (id) => `general/user/${id}`,
@@ -30,6 +30,11 @@ export const api = createApi({
             query: () => "client/geography",
             providesTags: ["Geography"]
         }),
+        getSales: build.query({ 
+            query: () => "sales/sales",
+            providesTags: [ "Sales" ]
+        })
     })
 })
-export const { useGetUserQuery, useGetProductsQuery, useGetCustomersQuery, useGetTransactionsQuery, useGetGeographyQuery } = api;
+export const { useGetUserQuery, useGetProductsQuery, useGetCustomersQuery, useGetTransactionsQuery, useGetGeographyQuery,
+    useGetSalesQuery } = api;
