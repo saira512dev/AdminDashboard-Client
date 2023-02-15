2900e9e42245203e49b1dd48e74b2bf6ec5c94f0
saira512dev <512saira@gmail.com>
2023-02-15 09:22:02 +0400

    Added routes to admin and performance pages

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import  Overview from "./scenes/overview";
 import  Daily from "./scenes/daily";
 import  Monthly from "./scenes/monthly";
 import  Breakdown from "./scenes/breakdown"
+import  Admin from "./scenes/admin"
+import  Performance from "./scenes/performance"
 
 function App() {
   const mode = useSelector((state) => state.global.mode)
@@ -34,7 +36,8 @@ function App() {
             <Route path="/daily" element={<Daily />} />
             <Route path="/monthly" element={<Monthly />} />
             <Route path="/breakdown" element={<Breakdown />} />
-
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/performance" element={<Performance />} />
           </Route>    
         </Routes>
       </ThemeProvider>
