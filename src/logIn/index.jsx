1e9570acebf928020c6232b599b1d9e1f3474111
saira512dev <512saira@gmail.com>
2023-02-20 10:00:00 -0600

    Added login form to login page

--- src/logIn/index.jsx
+++ src/logIn/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import FlexBetween from "../components/FlexBetween";
 import Header from "../components/Header";
+import Avatar from '@mui/material/Avatar';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import {
   DownloadOutlined,
   Email,
   PointOfSale,
   PersonAdd,
-  Traffic
+  Traffic,
 } from "@mui/icons-material";
 import {
   Box,
@@ -14,6 +16,9 @@ import {
   Typography,
   useTheme,
   useMediaQuery,
+  Link,
+  Grid,
+  TextField
 } from "@mui/material";
 
 
@@ -27,15 +32,73 @@ function Dashboard() {
   
 
   return (
-    <Box m="1.5rem 2.5rem" sx={{
+    <Box m="1.5rem 2rem" sx={{
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         height: "100%",
     }}>
-        <Box >
-        <Header title="Log In" subtitle="Login to your dashboard" />
-        
+        <Box  sx={{
+            backgroundColor: theme.palette.background.alt,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+        }}>
+        <Header title="SIGN IN" subtitle="Sign In to your dashboard" />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign In
+          </Typography>
+          <Box m="1rem" component="form" noValidate 
+          //onSubmit={handleSubmit} 
+          sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  //onChange={handleEmail}
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                 // onChange={handlePassword}
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              //onClick={handleSubmit}
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </Box>
         </Box>
       
           
