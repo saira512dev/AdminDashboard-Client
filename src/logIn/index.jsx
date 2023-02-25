13f7399e42cc85215cce549566db5386e2132e89
saira512dev <512saira@gmail.com>
2023-02-25 10:00:00 -0600

    Fixed bg color of text field by turning off autofill

--- src/logIn/index.jsx
+++ src/logIn/index.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 import { useState } from "react";
 import FlexBetween from "../components/FlexBetween";
 import Header from "../components/Header";
-import Avatar from '@mui/material/Avatar';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Avatar from "@mui/material/Avatar";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 import {
   DownloadOutlined,
@@ -23,12 +23,12 @@ import {
   TextField,
 } from "@mui/material";
 
-import { themeSettings } from '../theme';
+import { themeSettings } from "../theme";
 
 const Login = () => {
-// States for registration
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
+  // States for registration
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   // Handling the email change
   const handleEmail = (e) => {
@@ -40,64 +40,79 @@ const [password, setPassword] = useState("");
     setPassword(e.target.value);
   };
 
-  const theme = useTheme()
+  const theme = useTheme();
   const isNonMediumScreens = useMediaQuery("(min-width: 1200px)");
 
   return (
-    <Box m="1.5rem 2rem" sx={{
+    <Box
+      m="1.5rem 2rem"
+      sx={{
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         height: "100%",
-    }}>
-        <Box  pt="2rem" sx={{
-            backgroundColor: theme.palette.background.alt,
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            borderRadius: "0.2rem"
-        }}>
+      }}
+    >
+      <Box
+        pt="2rem"
+        sx={{
+          backgroundColor: theme.palette.background.alt,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius: "0.2rem",
+        }}
+      >
         <Header title="WELCOME" subtitle="Sign In to your dashboard" />
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'theme.palette.secondary' }}>
+          <Avatar sx={{ m: 1, bgcolor: "theme.palette.secondary" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign In
           </Typography>
-          <Box m="1rem" component="form" noValidate 
-          //onSubmit={handleSubmit} 
-          sx={{ mt: 3 }}>
+          <Box
+            m="1rem"
+            component="form"
+            noValidate
+            //onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
             <Grid container spacing={2}>
               <Grid item xs={12}>
                 <TextField
+                  inputProps={{
+                    autocomplete: "new-email",
+                    form: {
+                      autocomplete: "off",
+                    },
+                  }}
                   onChange={handleEmail}
                   required
                   fullWidth
                   id="email"
                   label="Email Address"
                   name="email"
-                  variant="filled"
+                  autoFocus
                 />
               </Grid>
               <Grid item xs={12}>
                 <TextField
-                 onChange={handlePassword}
+                  onChange={handlePassword}
                   required
                   fullWidth
                   name="password"
                   label="Password"
                   type="password"
                   id="password"
-                  variant="filled"
                 />
               </Grid>
             </Grid>
@@ -106,15 +121,15 @@ const [password, setPassword] = useState("");
               type="submit"
               fullWidth
               variant="contained"
-              sx={{ mt: 3, mb: 2, background: theme.palette.primary.main}}
+              sx={{ mt: 3, mb: 2, background: theme.palette.primary.main }}
             >
               Sign In
             </Button>
           </Box>
         </Box>
-        </Box>
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
