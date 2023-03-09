3f594c5cd9a9df89a7101518d1425ea78def4acd
saira512dev <512saira@gmail.com>
2023-03-09 17:50:56 +0400

    Changed API_URL to localhost

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-// const API_URL = "http://localhost:1212"
-const API_URL = "https://admin-dashboard-server-five.vercel.app"
+const API_URL = "http://localhost:5002"
+//const API_URL = "https://admin-dashboard-server-five.vercel.app"
 
 export default API_URL;
