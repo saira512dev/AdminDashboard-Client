68f7e9c26201e18b7971116ef8b53a2ebcc9a44e
saira512dev <512saira@gmail.com>
2023-03-10 13:02:58 +0400

    included credentials with api requests for authentication

--- src/state/api.js
+++ src/state/api.js
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: `${process.env.REACT_APP_BASE_URL}` }),
+    baseQuery: fetchBaseQuery({ baseUrl: `${process.env.REACT_APP_BASE_URL}`,
+                credentials: "include"
+            }),
     reducePath: "adminApi",
     tagTypes: ["User", "Products", "Customers", "Transactions", "Geography", "Sales", "Admins", "Performance", "Dashboard"],
     endpoints: (build) => ({
